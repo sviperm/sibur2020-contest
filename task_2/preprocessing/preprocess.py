9ef46a8582da781007d69d73097895a72d249c4a
Vladislav Sobolev <unsviperm@gmail.com>
2020-12-02 23:13:42 +0300

    Fix preprocessing

--- task_2/preprocessing/preprocess.py
+++ task_2/preprocessing/preprocess.py
@@ -14,32 +14,42 @@ def preprocess(company: str) -> str:
     company = company.lower()
 
     legal_entities = [r"ltd\.", r"co\.rporation", r"co\.", r"inc\.", r"b\.v\.",
-                      r"s\.c\.r\.l\.", r"gmbh", r"pvt\.", r"s\.a\.",
-                      r"de c\.v\.", r"c\.v\.", r"exp\.", r"llc", r"corp."
+                      r"s\.c\.r\.l\.", r"gmbh", r"pvt\.", r"imp\.",
+                      #   r"s\.a\.",
+                      r"de c\.v\.", r"c\.v\.", r"exp\.", r"llc", r"corp\.",
+                      r"ltda",
                       r"ооо", r"оао"]
+
     company_regexp = rf"({'|'.join(legal_entities)})[\W]*"
+
     company = re.sub(company_regexp, '', company)
 
-    company = re.sub(r'[^\w\s]', ' ', company)
+    company = re.sub(r'[^\w&\s]', '', company)
 
     popular_words = ["энтерпрайс", "ооо",
-                     "industries", "industrial", "industria",
-                     "international", "global", "logistics", "private",
-                     "corporation", "management", "consolidated", "trading"]
+                     "ltda", "de sa de cv",
+                     "industries", "industrial", "industria", "imperial",
+                     "international", "global", "logistics", "logistic",
+                     "private", "corporation", "management", "consolidated",
+                     "trading", ]
 
-    popular_regexp = rf"({'|'.join(popular_words)})[\W]*"
+    popular_regexp = rf"({'|'.join(popular_words + countries)})[\W]+"
     company = re.sub(popular_regexp, '', company)
 
-    countries_regexp = rf"({'|'.join(countries)})[\W]*"
-    company = re.sub(countries_regexp, '', company)
+    # countries_regexp = rf"({'|'.join(countries)})[\W]*"
+    # company = re.sub(countries_regexp, '', company)
 
     company = company.strip()
+    company = re.sub(r'\s&$', '', company)
     company = re.sub(r'\s{2,}', ' ', company)
 
     return company
 
 
+# %%
 if __name__ == "__main__":
+    # text = "Dongguan Silk Imp.& Exp.Co. Ltd.Of Guangdong"
+    # print(preprocess(text))
     ROOT = Path(__file__).parents[1] / "data"
     train = pd.read_csv(ROOT.joinpath("train.csv"), index_col="pair_id")
     train['name_1'] = train['name_1'].apply(preprocess)
@@ -52,12 +62,21 @@ if __name__ == "__main__":
     test.to_csv(ROOT / 'test_cleared.csv')
 
     # # %%
-    # df = pd.concat([train['name_1'], train['name_2']]).drop_duplicates()
-    # df = df.apply(lambda x: x.split(' '))
+    # ROOT = Path(__file__).parents[1] / "data"
+    # train = pd.read_csv(ROOT.joinpath("train_cleared.csv"), index_col="pair_id")
+    # column = pd.concat([train['name_1'], train['name_2']]).reset_index(drop=True).drop_duplicates()
+
+    # def fun(text):
+    #     try:
+    #         return str(text).split(' ')
+    #     except Exception:
+    #         return text
+
+    # column = column.apply(fun)
 
     # # %%
     # from itertools import chain
     # from collections import Counter
 
-    # counts = Counter(chain.from_iterable(df.to_list()))
+    # counts = Counter(chain.from_iterable(column.to_list()))
     # counts.most_common(100)
